95d627cc19ece0d2e2f267966a1e8a087b548c5f
Mundo Developer <developer@mundo.com>
2025-12-24 21:53:34 -0500

    fix: allow plus.unsplash.com images for hero background

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['ogjsblupoteatefyizgn.supabase.co', 'images.unsplash.com'],
+    domains: ['ogjsblupoteatefyizgn.supabase.co', 'images.unsplash.com', 'plus.unsplash.com'],
   },
 };
 
